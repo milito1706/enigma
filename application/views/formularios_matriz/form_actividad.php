903690343a14ab29ccbbc4d8326ddce5bc8a10ea
Isaac Vqz <isaacvqz@gmail.com>
2015-06-12 17:20:07 -0500

    Acomodo form actividad

--- application/views/formularios_matriz/form_actividad.php
+++ application/views/formularios_matriz/form_actividad.php
@@ -5,11 +5,6 @@
       <input type="hidden" name="id" id="id" value="<?php if(isset($Id)){echo $Id;}?>">
       <!--<input type="text" name="actividad_economica" id="actividad_economica" placeholder="Actividad Económica" class="form-control" value="<?php if(isset($actividad_economica)){echo $actividad_economica;}?>" readonly>-->
       <textarea name="actividad_economica" id="actividad_economica" class="form-control" readonly><?php if(isset($actividad_economica)){echo $actividad_economica;}?></textarea>
-    </div>    
-    </div> 
-    <div class="col-sm-4">
-    </div>
-    <div class="col-sm-4">
       <div class="form-group">
         <label>Calificación</label>
         <select name="calificacion" id="calificacion" class="form-control">
@@ -19,7 +14,7 @@
           <option value="3" <?php if (isset($calificacion) && !empty($calificacion == 3) ) echo 'selected="selected"'; ?> > 3 </option>
         </select>
       </div>
-    </div>
-    <div class="col-sm-4">
-    </div>
+    </div>    
+    </div> 
+    
 </div>
